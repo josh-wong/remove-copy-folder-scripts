70fc01cf78838af09566830b4878da66ec09b283
Josh Wong <joshuarwong@outlook.com>
2022-07-03 14:35:42 +0900

    Create copy-bot.py

---
+++ copy-bot.py
@@ -0,0 +1,19 @@
+# Python program to explain shutil.copytree() method 
+
+# importing shutil module 
+import shutil
+
+# Path: the parent folder where the files to be copied originate from
+# This path must end with a slash.
+path = '<INSERT FULL FOLDER PATH/>' 
+
+# Source path: where the files to be copied originate from
+src = '<INSERT FULL FOLDER PATH>'
+
+# Destination path: where the files should be copied to
+dest = '<INSERT FULL FOLDER PATH>'
+
+# Copy the content of source to destination 
+destination = shutil.copytree(src, dest) 
+
+# print(destination) prints the path of newly created file
